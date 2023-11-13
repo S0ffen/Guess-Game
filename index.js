63e45b23e599d4eea1108acef053ed35bd78e3fa
S0ffen <whitevolf314@gmail.com>
2023-11-13 02:59:13 +0100

    added Previous Guess

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var userInput = document.querySelector(".userInput")
 
+var previous = []
+
 var randomNumber = Math.ceil(Math.random()* 100)
+var Guesses = 0
 console.log(randomNumber)
+
+
+
 
 function startGameView(){
     document.querySelector(".selectDifficulty").style.display = "none"
@@ -15,20 +21,43 @@ function easyMode(){
 }
 
 userInput.addEventListener("keyup",function(e){
-    var textafterGuess = document.querySelector("h3")
-
-   
+    var textafterGuess = document.querySelector(".hint")
+    
     console.log(parseInt(userInput.value))
-    if(e.key === 'Enter' &&  parseInt(userInput.value) === randomNumber ) {
-        textafterGuess.classList.remove("guess")
+    if(e.key === "Enter" && userInput.value <= 0){
+        alert("dont leave blank input")
+    }
+
+    if(e.key ==="Enter"  && parseInt(userInput.value)>101){
+        alert("wrong number")
+    }
+    
+    if(e.key === 'Enter' && userInput.value > 0 && parseInt(userInput.value)<101){
+       previous.push(userInput.value)
+       document.querySelector(".previousNumber").classList.remove("hidden")
+       document.querySelector(".previousNumber").innerHTML = previous
+
+    
+    }
+    if(e.key === 'Enter' &&  parseInt(userInput.value) === randomNumber && Guesses != 15  ) {
+        textafterGuess.classList.remove("hidden")
         textafterGuess.innerHTML = "You Guess it"
+        Guesses ++
     }
-    else if(e.key === 'Enter' &&  parseInt(userInput.value) > randomNumber){
-        textafterGuess.classList.remove("guess")
+    else if(e.key === 'Enter' &&  parseInt(userInput.value) > randomNumber && Guesses != 15){
+        textafterGuess.classList.remove("hidden")
         textafterGuess.innerHTML = "Lower"
+        Guesses ++
     }
-    else if(e.key === 'Enter' && parseInt(userInput.value) < randomNumber){
-        textafterGuess.classList.remove("guess")
+    else if(e.key === 'Enter' && parseInt(userInput.value) < randomNumber && Guesses != 15){
+        textafterGuess.classList.remove("hidden")
         textafterGuess.innerHTML = "Higher"
+        Guesses ++
     }
+    else if((e.key === 'Enter' && parseInt(userInput.value) < randomNumber && Guesses == 15)){
+        textafterGuess.classList.remove("hidden")
+        textafterGuess.innerHTML = "You lose"
+    }
+    console.log(Guesses)
 })
+
